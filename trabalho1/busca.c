4d08d4b68f328913d5113a3dcb22699fc66e4873
romuluxs <romuluxs@gmail.com>
2016-05-13 20:03:32 -0300

    Tirando a leitura de arquivos do trabalho1

--- trabalho1/busca.c
+++ trabalho1/busca.c
@@ -41,22 +41,6 @@ int binary_search(int* vector, int vector_size, int value) {
     return -1;
 }
 
-// devolve o ponteiro para o arquivo
-FILE* get_file_pointer(char* file_name) {
-	FILE* file_pointer;
-
-	//abrindo o arquivo em modo "somente leitura"
-	file_pointer = fopen(file_name, "r");
-
-	// verifica se o arquivo conseguiu ser aberto
-	if(file_pointer == NULL) {
-		printf("Problemas na abertura do arquivo\n");
-		exit(EXIT_FAILURE);
-	}
-	// retorna o ponteiro
-	return file_pointer;
-}
-
 // aloca um vetor do tamanho passado por parametro
 int* create_vector(int size) {
 	int* vector = (int*)malloc(size*sizeof(int));
@@ -64,40 +48,26 @@ int* create_vector(int size) {
 }
 
 // lê o vetor do arquivo
-void read_vector(int* vector, int vector_size, FILE* file) {
+void read_vector(int* vector, int vector_size) {
 	int i;
 	for (i = 0; i < vector_size; ++i) {
-		fscanf(file, "%d ", &vector[i]);
+		scanf("%d ", &vector[i]);
 	}
-}
-
-// mostra os elementos do vetor
-void print_vector(int* vector, int vector_size) {
-	int i;
-	for (i = 0; i < vector_size; ++i) {
-		printf("%d\n", vector[i]);
-	}
-	printf("\n");
 }
 
 //------------ METODO PRINCIPAL --------------
-int main(int argc, char *argv[]) {
-	int SIZE = 100;
-	char line[SIZE];
+int main() {
 	int value, vector_size;
 
-	// criando a variável ponteiro para o arquivo
-	FILE* file = get_file_pointer(argv[1]);
-
 	// lê o valor a ser buscado e o tamanho do vetor do arquivo
-	fscanf(file, "%d\n", &value);
-	fscanf(file, "%d\n", &vector_size);
+	scanf("%d", &value);
+	scanf("%d", &vector_size);
 
 	// aloca o vetor
 	int* vector = create_vector(vector_size);
 
 	// lê os dados do arquivo
-	read_vector(vector, vector_size, file);
+	read_vector(vector, vector_size);
 
 	// resultados da busca sequencial
 	printf("\n------ BUSCA SEQUENCIAL ------\n");
@@ -121,11 +91,6 @@ int main(int argc, char *argv[]) {
 
 	printf("\n");
 
-	// mostra os elementos do vetor
-	//print_vector(vector, vector_size);
-
 	free(vector); // desaloca da memoria
-	fclose(file); // fecha o arquivo	 
-
 	return 0;
 }
